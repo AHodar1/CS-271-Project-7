f94dec9798028984647ebd43ac73e373563c3d16
AHodar1 <hodar_k1@denison.edu>
2025-04-22 20:59:47 -0400

    studentTests constructor and vertexIn

--- studentTests.cpp
+++ studentTests.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
-    
+
+    Graph g1(4);
+    cout << "construst g1(4)" << endl;
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "g1.vertexIn(" << i << ") = " << g1.vertexIn(i) << endl;
+    }
     return 0;
 }
