513f671acee2baf3645e329537196dd77bc2d079
AHodar1 <hodar_k1@denison.edu>
2025-04-22 21:15:16 -0400

    studentTests bad edgeIn testing

--- studentTests.cpp
+++ studentTests.cpp
@@ -9,5 +9,12 @@ int main()
     {
         cout << "g1.vertexIn(" << i << ") = " << g1.vertexIn(i) << endl;
     }
+    for (int i = 0; i < 4; i++)
+    {
+        for (int n = 0; n < 4; n++)
+        {
+            cout << "g1.edgeIn(" << i << ", " << n << ") = " << g1.edgeIn(i, n) << endl;
+        }
+    }
     return 0;
 }
